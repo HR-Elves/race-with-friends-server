d31dbc9a8a857904c0f6f9008a393741c3545d7f
sscchan <stephen.s.c.chan@gmail.com>
2017-02-20 12:06:11 -0800

    add statuschecking of Users and Runs services

--- Router/index.js
+++ Router/index.js
@@ -7,17 +7,51 @@ const request = require('request');
 
 
 app.get('/', function (req, res) {
-  request('http://runservice:5000', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      res.send('RouterService Reached! Runs Services Says: ' +  body); // Show the HTML for the Google homepage.
+  console.log('GET /');
+  let statusCheckMessage = '';
+
+  checkRunsService(function(err, data) {
+    if (err) {
+      statusCheckMessage += 'RunsService Error: ' + err.message + '\n |';
     } else {
-      res.statusCode = 200;
-      res.send(error);
+      statusCheckMessage += data + '\n |';
+      checkUsersService(function(err, data) {
+        if (err) {
+          statusCheckMessage += 'UsersService Error: ' + err.message + '\n |';
+        } else {
+          statusCheckMessage += data + '\n |';
+          res.status(200).send(statusCheckMessage);
+          res.end();
+        }
+      });
     }
-  })
+  });
 
   // res.send('CONTAINERIZED EXPERIMENT SUCCESSFUL')
 });
+
+function checkRunsService(callback) {
+  console.log('Checking RunsService...');
+  request({url: 'http://runsservice:5000', timeout: 1000}, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      callback(null, 'RunsServiceReached! Service Says: ' + body);
+    } else {
+      callback(new Error(error), null);
+    }
+  });
+}
+
+function checkUsersService(callback) {
+  console.log('Checking UsersService...');  
+  request({url: 'http://usersservice:5000', timeout: 1000}, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      callback(null, 'UsersService! Service Says: ' + body);
+    } else {
+      callback(new Error(error), null);
+    }
+  });
+}
+
 
 app.listen(port, function() {
   console.log('Example app listening on port: ', port);
