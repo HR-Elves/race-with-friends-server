3b3dd1319e6d82b4a8f9be7fc84a719d6007b2a4
sscchan <stephen.s.c.chan@gmail.com>
2017-02-23 14:10:02 -0800

    implement DataPoint method to return it's own properties as a Dict

--- RunsService/app/dbmodels.py
+++ RunsService/app/dbmodels.py
@@ -4,31 +4,49 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
 
 class Run(db.Model):
-  id = db.Column(db.BigInteger, primary_key=True)
-  name = db.Column(db.Text)
-  description = db.Column(db.Text)
-  length = db.Column(db.Float)
-  duration = db.Column(db.BigInteger)
-  created_on = db.Column(db.Text)
-  user_id = db.Column(db.Text, index=True)
-  data_points = db.relationship('DataPoint', backref='run', lazy='dynamic')
+    id = db.Column(db.BigInteger, primary_key=True)
+    name = db.Column(db.Text)
+    description = db.Column(db.Text)
+    length = db.Column(db.Float)
+    duration = db.Column(db.BigInteger)
+    created_on = db.Column(db.Text)
+    user_id = db.Column(db.Text, index=True)
+    data_points = db.relationship('DataPoint', backref='run', lazy='dynamic')
 
-  def __str__(self):
-      displayString = ''
-      displayString += 'db_id: %d \n' % self.id
-      displayString += 'name: %s \n' % self.name
-      displayString += 'description: %s \n' % self.description
+    def __str__(self):
+        displayString = ''
+        displayString += 'db_id: %d \n' % self.id
+        displayString += 'name: %s \n' % self.name
+        displayString += 'description: %s \n' % self.description
 
-      return displayString
+        return displayString
+
+
 
 class DataPoint(db.Model):
-  id = db.Column(db.BigInteger, primary_key=True)
-  latitude = db.Column(db.Float)
-  longitude = db.Column(db.Float)
-  altitude = db.Column(db.Float)
-  timestamp = db.Column(db.Text)
-  time_delta = db.Column(db.Integer)
-  time_total = db.Column(db.BigInteger)
-  dist_delta = db.Column(db.Float)
-  dist_total = db.Column(db.Float)
-  run_id = db.Column(db.BigInteger, db.ForeignKey('run.id'))
+    id = db.Column(db.BigInteger, primary_key=True)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    altitude = db.Column(db.Float)
+    timestamp = db.Column(db.Text)
+    time_delta = db.Column(db.Integer)
+    time_total = db.Column(db.BigInteger)
+    distance_delta = db.Column(db.Float)
+    distance_total = db.Column(db.Float)
+    run_id = db.Column(db.BigInteger, db.ForeignKey('run.id'))
+
+    def as_Dict(self):
+        selfAsDict = {
+          'lat' : self.latitude,
+          'long' : self.longitude,
+          'altitude' : self.altitude,
+          'timestamp' : self.timestamp,
+          'timeDelta' : self.distance_delta,      
+          'timeTotal' : self.time_total,
+          'distanceDelta' : self.distance_delta,
+          'distanceTotal' : self.distance_total,
+        }
+        return selfAsDict
+
+
+
