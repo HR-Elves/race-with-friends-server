7124f0a98a10efa871f2f0cab8b1b4e4524ef637
sscchan <stephen.s.c.chan@gmail.com>
2017-03-02 13:13:06 -0800

    modify dbmodels to retrieve run data as part of GET challenges

--- ChallengesService/app/dbmodels.py
+++ ChallengesService/app/dbmodels.py
@@ -1,5 +1,7 @@
 from main import app
 from flask_sqlalchemy import SQLAlchemy
+import requests
+
 
 db = SQLAlchemy(app)
 
@@ -24,10 +26,19 @@ class Challenge(db.Model):
             'opponents' : []
         }
 
+        # Retrieve and populate opponents
         challenge_opponents = Challenge_Opponents.query.filter_by(challenge_id=self.id).all()
         if challenge_opponents is not None:
             for challenge in challenge_opponents:
                 selfAsDict['opponents'].append(challenge.opponent_id)
+
+        # Retrieve run information associated with the challenge
+        response = requests.get('http://runsservice:80/runs/%s' %self.run_id)
+        if response.status_code == 200:
+            response_data = response.json()
+            selfAsDict['challenger_name'] = response_data.get('name')
+            selfAsDict['distanceTotal'] = response_data.get('length')
+            selfAsDict['timeTotal'] = response_data.get('duration')
 
         return selfAsDict
 
